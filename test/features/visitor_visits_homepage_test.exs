22b8bb4cef8d378e1f8c9059ddd43e0057596095
Josh Branchaud and Jake Worth <dev+jbranchaud+jwworth@hashrocket.com>
2016-11-18 15:02:05 -0600

    Add and test site header

--- test/features/visitor_visits_homepage_test.exs
+++ test/features/visitor_visits_homepage_test.exs
@@ -1,9 +1,10 @@
 defmodule VisitorVisitsHomepageTest do
   use TodayILearned.IntegrationCase
 
-  test "the page title is TIL" do
+  test "the page has the appropriate branding" do
     navigate_to("/")
 
     assert page_title == "Today I Learned"
+    assert visible_in_element?({:tag, "h1"}, ~r/Today I Learned/)
   end
 end
