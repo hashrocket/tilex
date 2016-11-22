2b11860b99ab324d6f359352995ec4fffd5f20ff
Jake Worth <jake@jakeworth.com>
2016-11-21 22:48:28 -0600

    Add Hashrocket logo to header

--- lib/today_i_learned/endpoint.ex
+++ lib/today_i_learned/endpoint.ex
@@ -9,7 +9,7 @@ defmodule TodayILearned.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :today_i_learned, gzip: false,
-    only: ~w(css fonts images js favicon.png favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.png favicon.ico hashrocket-log.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
