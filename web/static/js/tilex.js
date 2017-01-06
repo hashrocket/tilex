32d09bd56f3edd1b8fd7788815c1b2f03314e90e
Jake Worth <jake@jakeworth.com>
2017-01-05 21:10:27 -0600

    Add informational sidebar

--- web/static/js/tilex.js
+++ web/static/js/tilex.js
@@ -4,4 +4,11 @@ $(function(){
     e.preventDefault();
     $(this).fadeOut(200);
   });
+
+  $(".site_nav__search, .site_nav__about").on("click", ".site_nav__link", function(e) {
+    e.preventDefault();
+    $(this).closest('li')
+      .toggleClass('site_nav--open')
+      .find(":input:visible").eq(0).focus();
+  });
 });
