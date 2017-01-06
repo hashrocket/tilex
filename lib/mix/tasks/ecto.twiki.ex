e13201fc119f3b36d01f069311f3bc9ef3055315
Chris Erin and Jake Worth <dev+chriserin+jwworth@hashrocket.com>
2017-01-06 15:28:10 -0600

    Add Josh's @jbranchaud twiki module
    https://hashrocket.com/blog/posts/up-down-up-with-ecto-migrations

---
+++ lib/mix/tasks/ecto.twiki.ex
@@ -0,0 +1,64 @@
+defmodule Mix.Tasks.Ecto.Twiki do
+  use Mix.Task
+
+  @shortdoc "Ecto Migration: Up, Down, Up"
+
+  @moduledoc """
+    This will migrate the latest Ecto migration, roll it back, and then
+    migrate it again. This is an easy way to ensure that migrations can go
+    up and down.
+  """
+
+  def run(args) do
+    Application.ensure_all_started(Mix.Project.config[:app])
+
+    repos = Mix.Ecto.parse_repo(args)
+
+    twiki(repos)
+  end
+
+  defp twiki(repo) when is_atom(repo) do
+    migration_dir =
+      repo
+      |> Mix.Ecto.source_repo_priv
+      |> Path.absname
+      |> Path.join("migrations")
+
+    count = down_count(repo, migration_dir)
+
+    Enum.each([:up, :down, :up], fn(direction) ->
+      migrate(direction, repo, migration_dir, [step: count])
+    end)
+  end
+
+  defp twiki([repo]) do
+    twiki(repo)
+  end
+
+  defp twiki([_repo | _more_repos] = repos) do
+    Mix.shell.info """
+      Ecto.Twiki only supports migrating a single repo.
+      However, we found multiple repos: #{inspect repos}
+    """
+  end
+
+  defp migrate(direction, _repo, _migration_dir, [step: 0]) do
+    Mix.shell.info "Already #{direction}"
+    []
+  end
+
+  defp migrate(direction, repo, migration_dir, opts) do
+    Mix.shell.info "Migrating #{direction}"
+    Ecto.Migrator.run(repo, migration_dir, direction, opts)
+  end
+
+  defp down_count(repo, migration_dir) do
+    direction_count(:down, repo, migration_dir)
+  end
+
+  defp direction_count(direction, repo, migration_dir) do
+    Ecto.Migrator.migrations(repo, migration_dir)
+    |> Enum.filter(fn({status, _, _}) -> status == direction end)
+    |> Enum.count
+  end
+end
