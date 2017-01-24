2f6761ef396fa0ea3810c105862ac526669000f4
Jake Worth <jake@jakeworth.com>
2017-01-23 23:55:10 -0600

    Add ability to sign in via Google Oauth

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,15 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :ueberauth, Ueberauth,
+  providers: [
+    google: {Ueberauth.Strategy.Google, [approval_prompt: "force", access_type: "offline"]}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_CLIENT_ID"),
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
