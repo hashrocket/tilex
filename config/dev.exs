cdb7b112b511fbf3d9b97f50f894574468b6b797
jbranchaud <jbranchaud@gmail.com>
2017-03-17 14:06:51 -0500

    Allow specifying the server port with an env var

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :tilex, Tilex.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
