ef7aff580ea0d538a87b567f5de4408b52d23186
Jake Worth <jake@jakeworth.com>
2017-03-19 18:37:38 -0500

    Calculate total posts in developer's header

--- test/features/visitor_views_developer_test.exs
+++ test/features/visitor_views_developer_test.exs
@@ -16,4 +16,19 @@ defmodule Features.VisitorViewsDeveloper do
     assert page_header =~ ~r/1 post by makinpancakes/
     assert find(session, Query.css("article.post"))
   end
+
+  test "and sees a prolific developer's posts", %{session: session} do
+
+    developer = Factory.insert!(:developer, username: "banjocardhush")
+
+    1..55
+      |> Enum.each(fn(_i) ->
+        Factory.insert!(:post, developer: developer)
+      end)
+
+    visit(session, developer_path(Endpoint, :show, developer))
+
+    page_header = Element.text(find(session, Query.css(".page_head")))
+    assert page_header =~ ~r/55 posts by banjocardhush/
+  end
 end
--- web/controllers/developer_controller.ex
+++ web/controllers/developer_controller.ex
@@ -4,8 +4,13 @@ defmodule Tilex.DeveloperController do
   def show(conn, %{"name" => username} = params) do
     page = Map.get(params, "page", "1") |> String.to_integer
 
-    {posts, developer} = Tilex.Posts.by_developer(username, page)
+    {posts, posts_count, developer} = Tilex.Posts.by_developer(username, page)
 
-    render(conn, "show.html", developer: developer, posts: posts, page: page)
+    render(conn, "show.html",
+      posts: posts,
+      posts_count: posts_count,
+      developer: developer,
+      page: page
+    )
   end
 end
