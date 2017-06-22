09d903b6e13af87563fa154e75136e201ba4d955
Jake Worth <jake@jakeworth.com>
2017-06-21 19:43:24 -0500

    Add a twitter button to post show page #67

--- web/web.ex
+++ web/web.ex
@@ -44,7 +44,11 @@ defmodule Tilex.Web do
       use Phoenix.View, root: "web/templates"
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_csrf_token: 0,
+        get_flash: 2,
+        view_module: 1,
+        action_name: 1
+      ]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
