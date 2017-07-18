3c9e2418aa8f1ba453c960ff37af343c94a60732
Taylor Mock <tmock12@gmail.com>
2017-07-18 23:22:07 +0300

    Links use full post slug from stats page

--- web/controllers/stats_controller.ex
+++ web/controllers/stats_controller.ex
@@ -2,10 +2,8 @@ defmodule Tilex.StatsController do
   use Tilex.Web, :controller
 
   def index(conn, _params) do
-    Plug.Conn.assign(conn, :page_title, "Statistics")
-    |> render(
-           "index.html",
-           Tilex.Stats.all
-         )
+    conn
+    |> assign(:page_title, "Statistics")
+    |> render("index.html", Tilex.Stats.all)
   end
 end
