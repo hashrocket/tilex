f5b45644d3fc0ee7e998857a2457e0e59b6fc2d2
Taylor Mock <tmock12@gmail.com>
2017-07-18 22:41:35 +0300

    Fix unnecessary conn assignment in sitemap controller

--- web/controllers/sitemap_controller.ex
+++ web/controllers/sitemap_controller.ex
@@ -2,8 +2,9 @@ defmodule Tilex.SitemapController do
   use Tilex.Web, :controller
 
   def index(conn, _) do
-    conn = Plug.Conn.assign(conn, :posts, Repo.all(Tilex.Post))
-    conn = put_layout conn, false
-    conn = render conn, "sitemap.xml"
+    conn
+    |> assign(:posts, Repo.all(Tilex.Post))
+    |> put_layout(false)
+    |> render("sitemap.xml")
   end
 end
