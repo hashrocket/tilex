7b7f9425c42826b5b520be0860c2fae41f417cfb
Taylor Mock <tmock12@gmail.com>
2017-07-21 14:56:24 +0300

    Use Phoenix render_many for developer post api JSON

--- web/controllers/api/developer_post_controller.ex
+++ web/controllers/api/developer_post_controller.ex
@@ -4,6 +4,6 @@ defmodule Tilex.Api.DeveloperPostController do
   def index(conn, params) do
     posts = Tilex.Posts.by_developer(params["username"], limit: 3)
 
-    render conn, "index.json", posts: posts
+    render(conn, "index.json", posts: posts)
   end
 end
