2de43746b67852f4119712831bbdde066bcfbce5
Chris Erin and Jake Worth <dev+chriserin+jwworth@hashrocket.com>
2017-08-11 14:01:38 -0500

    Upgrade to Elixir 1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tilex.Mixfile do
   def project do
     [app: :tilex,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
