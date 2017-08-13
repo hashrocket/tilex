566736963296f697747257e43a6dcb062e964924
Chris Erin <chris.erin@gmail.com>
2017-08-13 14:06:18 -0500

    Configure the Endpoint host from the env

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tilex, TilexWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "til.hashrocket.com", port: 80],
+  url: [host: System.get_env("HOST"), port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
