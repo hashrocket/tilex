d4d0b49f032091ae3ebf5ac427e8b6e67252b801
Jake Worth <jake@jakeworth.com>
2017-08-15 21:53:15 -0500

    Move aliases inline

--- lib/guardian_serializer.ex
+++ lib/guardian_serializer.ex
@@ -1,8 +1,7 @@
 defmodule Tilex.GuardianSerializer do
   @behaviour Guardian.Serializer
 
-  alias Tilex.Repo
-  alias Tilex.Developer
+  alias Tilex.{Developer, Repo}
 
   def for_token(developer = %Developer{}), do: { :ok, "Developer:#{developer.id}" }
   def for_token(_), do: { :error, "Unknown resource type" }
