837b36dca32ba9bc19690684f142309f937e0297
Jake Worth <jake@jakeworth.com>
2017-08-22 12:12:55 -0500

    Move pattern matched variables to the right
    The previous code was correct; this brings it in line under one
    standard. See:
    
    https://github.com/rrrene/credo/blob/master/lib/credo/check/consistency/parameter_pattern_matching.ex

--- lib/guardian_serializer.ex
+++ lib/guardian_serializer.ex
@@ -3,7 +3,7 @@ defmodule Tilex.GuardianSerializer do
 
   alias Tilex.{Developer, Repo}
 
-  def for_token(developer = %Developer{}), do: {:ok, "Developer:#{developer.id}"}
+  def for_token(%Developer{} = developer), do: {:ok, "Developer:#{developer.id}"}
   def for_token(_), do: {:error, "Unknown resource type"}
 
   def from_token("Developer:" <> id), do: {:ok, Repo.get(Developer, id)}
