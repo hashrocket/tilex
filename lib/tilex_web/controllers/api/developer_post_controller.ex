fe8f4c4cdc85e275a438de6eb12659ee2e263164
Jake Worth <jake@jakeworth.com>
2017-08-22 10:16:14 -0500

    Document our user-facing API module and function

--- lib/tilex_web/controllers/api/developer_post_controller.ex
+++ lib/tilex_web/controllers/api/developer_post_controller.ex
@@ -1,8 +1,17 @@
 defmodule TilexWeb.Api.DeveloperPostController do
-  use TilexWeb, :controller
+  @moduledoc """
+  This module implements a public API for querying Tilex data.
+  """
 
+  use TilexWeb, :controller
+  alias Tilex.{Posts}
+
+  @doc """
+  This function allows external requesters to retrieve a developer's three most
+  recent posts.
+  """
   def index(conn, params) do
-    posts = Tilex.Posts.by_developer(params["username"], limit: 3)
+    posts = Posts.by_developer(params["username"], limit: 3)
 
     render(conn, "index.json", posts: posts)
   end
