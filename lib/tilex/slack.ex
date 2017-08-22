7e8b724f46fed64fb0680ff81f449945583b8046
Jake Worth <jake@jakeworth.com>
2017-08-22 11:43:49 -0500

    Convert emoji to Slack shortcodes

--- lib/tilex/slack.ex
+++ lib/tilex/slack.ex
@@ -1,12 +1,15 @@
 defmodule Tilex.Slack do
+
+  @emoji ~w(:tada: :birthday: :sparkles: :boom: :hearts: :balloon: :crown: :mortar_board: :trophy: :100:)
+
   def notify(post, developer, channel, url) do
     "#{developer.username} created a new post <#{url}|#{post.title}> ##{channel.name}"
     |> send_slack_message
   end
 
   def notify_of_awards(%Tilex.Post{max_likes: max_likes, title: title}, developer, url) do
-    appropriate_emoji = [ "🎉", "🎂", "✨", "💥", "❤️", "🎈", "👑", "🎓", "🏆", "💯 "]
-                        |> Enum.at(round((max_likes / 10) - 1), "😀")
+    appropriate_emoji = @emoji
+    |> Enum.at(round((max_likes / 10) - 1), ":smile:")
 
     "#{developer.username}'s post has #{max_likes} likes! #{appropriate_emoji} - <#{url}|#{title}>"
     |> send_slack_message
