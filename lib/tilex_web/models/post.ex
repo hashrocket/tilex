94444572dfad9509c3f97838590c5fd0b65fa6b0
Jake Worth <jake@jakeworth.com>
2017-08-23 11:09:26 -0500

    Abstract post permitted/required params

--- lib/tilex_web/models/post.ex
+++ lib/tilex_web/models/post.ex
@@ -5,8 +5,13 @@ defmodule Tilex.Post do
 
   @body_max_words 200
   def body_max_words, do: @body_max_words
+
   @title_max_chars 50
   def title_max_chars, do: @title_max_chars
+
+  @params ~w(title body developer_id channel_id likes max_likes)a
+  def permitted_params, do: @params
+  def required_params,  do: @params
 
   schema "posts" do
     field :title, :string
@@ -22,24 +27,21 @@ defmodule Tilex.Post do
     timestamps()
   end
 
-  def slugified_title(title) do
-    title
-      |> String.downcase
-      |> String.replace(~r/[^A-Za-z0-9\s-]/, "")
-      |> String.replace(~r/(\s|-)+/, "-")
-  end
-
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:title, :body, :developer_id, :channel_id, :likes, :max_likes])
-    |> validate_required([:title, :body, :developer_id, :channel_id, :likes, :max_likes])
-    |> validate_length(:title, max: @title_max_chars)
+    |> cast(params, permitted_params())
+    |> validate_required(required_params())
+    |> validate_length(:title, max: title_max_chars())
     |> validate_length_of_body
     |> validate_number(:likes, greater_than: 0)
     |> add_slug
+  end
+
+  def slugified_title(title) do
+    title
+    |> String.downcase
+    |> String.replace(~r/[^A-Za-z0-9\s-]/, "")
+    |> String.replace(~r/(\s|-)+/, "-")
   end
 
   defp validate_length_of_body(changeset) do
