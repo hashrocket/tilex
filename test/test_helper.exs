37c2d2c5fb26cc4b9a7043fa6dd43f0250b67418
Antonin Kral <a.kral@bobek.cz>
2017-09-21 14:58:49 +0200

    Wallaby doesn't need to have URL hard-coded

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 ExUnit.start
 
 {:ok, _} = Application.ensure_all_started(:wallaby)
-Application.put_env(:wallaby, :base_url, "http://localhost:4001")
+Application.put_env(:wallaby, :base_url, TilexWeb.Endpoint.url)
