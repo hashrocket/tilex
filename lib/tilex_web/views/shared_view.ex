4506a67729a76d9aaa459158f6bc066edd1a30f3
jbranchaud <jbranchaud@gmail.com>
2017-10-29 13:32:42 -0500

    Use timex to convert dates to Chicago timezone
    This fixes the issue described in #185

--- lib/tilex_web/views/shared_view.ex
+++ lib/tilex_web/views/shared_view.ex
@@ -2,11 +2,20 @@ defmodule TilexWeb.SharedView do
   use TilexWeb, :view
 
   def display_date(post) do
-    Timex.format!(post.inserted_at, "%B %-e, %Y", :strftime)
+    post.inserted_at
+    |> datetime_in_zone()
+    |> Timex.format!("%B %-e, %Y", :strftime)
   end
 
   def rss_date(post) do
-    Timex.format!(post.inserted_at, "%a, %d %b %Y %H:%M:%S GMT", :strftime)
+    post.inserted_at
+    |> datetime_in_zone()
+    |> Timex.format!("%a, %d %b %Y %H:%M:%S GMT", :strftime)
+  end
+
+  defp datetime_in_zone(datetime) do
+    timezone = Timex.Timezone.get("America/Chicago", datetime)
+    Timex.Timezone.convert(datetime, timezone)
   end
 
   def pluralize(1, object), do: "1 #{object}"
