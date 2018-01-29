696148bd947aedb1dcc25a205352cdd8e0195b95
Jake Worth <jake@jakeworth.com>
2018-01-29 07:33:28 -0600

    Autoformat lib/tilex_web/endpoint.ex

--- lib/tilex_web/endpoint.ex
+++ lib/tilex_web/endpoint.ex
@@ -2,18 +2,16 @@ defmodule TilexWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :tilex
 
   if Application.get_env(:tilex, :sql_sandbox) do
-    plug Phoenix.Ecto.SQL.Sandbox
+    plug(Phoenix.Ecto.SQL.Sandbox)
   end
 
-  socket "/socket", TilexWeb.UserSocket
+  socket("/socket", TilexWeb.UserSocket)
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :tilex, gzip: false,
-    only: ~w(assets
+  plug(Plug.Static, at: "/", from: :tilex, gzip: false, only: ~w(assets
       apple-touch-icon-120x120.png
       apple-touch-icon.png
       apple-touch-icon-precomposed.png
@@ -25,35 +23,39 @@ defmodule TilexWeb.Endpoint do
       images
       js
       robots.txt
-    )
+    ))
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.RequestId
-  plug Plug.Logger
+  plug(Plug.RequestId)
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(
+    Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Poison
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(
+    Plug.Session,
     store: :cookie,
     key: "_tilex_key",
     signing_salt: "PSNTTaPr"
+  )
 
   use Appsignal.Phoenix
-  plug TilexWeb.Router
+  plug(TilexWeb.Router)
 end
