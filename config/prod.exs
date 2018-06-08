1b91b8836437d617651a207fc2aa6f3b836707af
Jake Worth <jake@jakeworth.com>
2018-06-08 16:50:09 -0500

    Update static manifest location
    https://gist.github.com/chrismccord/71ab10d433c98b714b75c886eff17357

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :tilex, TilexWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("HOST"), port: 80],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
