38d301f0929baba537f0772a570e741a1a9acfca
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-07-02 11:55:03 -0400

    🔧  Configure Appsignal.Ecto through telemetry

--- lib/tilex.ex
+++ lib/tilex.ex
@@ -18,6 +18,13 @@ defmodule Tilex do
       supervisor(Tilex.Notifications.NotifiersSupervisor, [])
     ]
 
+    :telemetry.attach(
+      "appsignal-ecto",
+      [:tilex, :repo, :query],
+      &Appsignal.Ecto.handle_event/4,
+      nil
+    )
+
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Tilex.Supervisor]
