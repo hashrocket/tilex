63047ee099e014c9ea0d578631c412fb74476e62
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-07-02 14:19:28 -0400

    ✅  Add some Markdown tests for to_html_live and to_html

--- test/lib/tilex/markdown_test.exs
+++ test/lib/tilex/markdown_test.exs
@@ -1,13 +1,43 @@
 defmodule Lib.Tilex.MarkdownTest do
   use ExUnit.Case, async: true
 
+  alias Tilex.Markdown
+
+  @to_html_data [
+    {"", ""},
+    {"Some text", "<p>Some text</p>"},
+    {"```\nsome code block\n```", "<pre><code class=\" language-\">some code block</code></pre>"},
+    {"```elixir\ndefmodule Foo do\nend\n```", "<pre><code class=\"elixir language-elixir\">defmodule Foo do\nend</code></pre>"},
+    {"<script>alert('A great grasshopper!')</script>", "<p>alert(‘A great grasshopper!’)</p>"},
+    {"Some http://link.com?foo=bar", "<p>Some http://link.com?foo=bar</p>"},
+    {"some [Link](http://link.com?foo=bar)", "<p>some <a href=\"http://link.com?foo=bar\">Link</a></p>"},
+    {"some [Relative Link](/link.com?foo=bar)", "<p>some <a href=\"/link.com?foo=bar\">Relative Link</a></p>"},
+    {"```\n# some http://link.com?foo=bar\n```", "<pre><code class=\" language-\"># some http://link.com?foo=bar</code></pre>"},
+  ]
+
+  describe "to_html_live/1" do
+    for {value, expected} <- @to_html_data do
+      @value value
+      @expected expected
+
+      test "converts markdown '#{inspect(@value)}' into html" do
+        assert Markdown.to_html_live(@value) == @expected
+      end
+    end
+  end
+
   describe "to_html/1" do
+    for {value, expected} <- @to_html_data do
+      @value value
+      @expected expected
+
+      test "converts markdown '#{inspect(@value)}' into html" do
+        assert Markdown.to_html(@value) == @expected
+      end
+    end
+
     test "removes script tags" do
-      html =
-        """
-        <script>alert('A great grasshopper!')</script>
-        """
-        |> Tilex.Markdown.to_html()
+      html = Markdown.to_html("<script>alert('A great grasshopper!')</script>")
 
       assert html == "<p>alert(‘A great grasshopper!’)</p>"
     end
@@ -19,7 +49,7 @@ defmodule Lib.Tilex.MarkdownTest do
         """
           <a href='/relative' id='1'>my relatives</a>
         """
-        |> Tilex.Markdown.expand_relative_links("https://til.hashrocket.com")
+        |> Markdown.expand_relative_links("https://til.hashrocket.com")
 
       expected = """
         <a href="https://til.hashrocket.com/relative" id="1">my relatives</a>
@@ -33,7 +63,7 @@ defmodule Lib.Tilex.MarkdownTest do
         """
           <a href='http://elixirdocs.org/strings' id='1'>my relatives</a>
         """
-        |> Tilex.Markdown.expand_relative_links("https://til.hashrocket.com")
+        |> Markdown.expand_relative_links("https://til.hashrocket.com")
 
       expected = """
         <a href="http://elixirdocs.org/strings" id="1">my relatives</a>
@@ -47,7 +77,7 @@ defmodule Lib.Tilex.MarkdownTest do
         """
           <a href='https://elixirdocs.org/strings' id='1'>my relatives</a>
         """
-        |> Tilex.Markdown.expand_relative_links("https://til.hashrocket.com")
+        |> Markdown.expand_relative_links("https://til.hashrocket.com")
 
       expected = """
         <a href="https://elixirdocs.org/strings" id="1">my relatives</a>
