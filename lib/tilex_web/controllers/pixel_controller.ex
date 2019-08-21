d0af15b712bb1e1a6f532ffbbb6a42bf75fe7d8e
Dorian Karter <dkarter@users.noreply.github.com>
2019-08-21 15:51:36 -0500

    Fix tracking pixel mime-type

--- lib/tilex_web/controllers/pixel_controller.ex
+++ lib/tilex_web/controllers/pixel_controller.ex
@@ -6,6 +6,7 @@ defmodule TilexWeb.PixelController do
 
     conn
     |> put_resp_header("cache-control", "no-cache, no-store, must-revalidate")
+    |> put_resp_content_type("application/javascript")
     |> send_resp(:ok, "")
   end
 end
