2e0c79f8c0ecc6b4274a541251f0e84c921446fd
Dorian Karter <dkarter@users.noreply.github.com>
2019-09-27 01:14:22 -0500

    Use ETS for RateLimiter
    This removes the bottleneck of one process serving as a gateway for all
    requests and is inspired by Chris McCords' Programming Phoenix >= 1.4:
    
    “If all requests depend on a single process for caching, said process will become a bottleneck and hurt the user experience.”
    
    Excerpt From: Chris McCord, Bruce Tate, José Valim. “Programming Phoenix ≥ 1.4 (for Dorian Karter).” Apple Books.
    Chapter 12: OTP, Section: Designing an Information System with OTP

--- lib/tilex/rate_limiter.ex
+++ lib/tilex/rate_limiter.ex
@@ -5,38 +5,53 @@ defmodule Tilex.RateLimiter do
   @date_time_module Application.get_env(:tilex, :date_time_module)
   @limit Application.get_env(:tilex, :rate_limiter_requests_per_time_period)
   @time_period_minutes Application.get_env(:tilex, :rate_limiter_time_period_minutes)
-
-  # Client ----------------------------------------------
+                       |> Timex.Duration.from_minutes()
+  @table_name :rate_limiter_lookup
 
   def start_link do
     GenServer.start_link(__MODULE__, [], name: @name)
   end
 
+  @spec check(ip: String.t()) :: boolean()
   def check(ip: ip) do
-    GenServer.call(@name, {:check_ip, ip})
-  end
+    request_times = lookup_ip(ip)
+    current_time = @date_time_module.utc_now()
+    recent_request_times = filter_recent_request_times(current_time, request_times)
+    current_requests = [current_time | recent_request_times]
 
-  # Server ----------------------------------------------
+    :ets.insert(@table_name, {ip, current_requests})
+
+    length(current_requests) <= @limit
+  end
 
   @impl true
   def init(_) do
+    create_table()
     {:ok, %{}}
   end
 
-  @impl true
-  def handle_call({:check_ip, ip}, _from, state) do
-    request_times = state |> Map.get(ip, [])
-    current_time = @date_time_module.utc_now()
-    end_time = Timex.subtract(current_time, Timex.Duration.from_minutes(@time_period_minutes))
+  defp create_table do
+    :ets.new(@table_name, [
+      :set,
+      :public,
+      :named_table,
+      read_concurrency: true,
+      write_concurrency: true
+    ])
+  end
 
-    recent_request_times =
-      Enum.filter(request_times, fn time ->
-        Timex.between?(time, end_time, current_time, inclusive: true)
-      end)
+  defp lookup_ip(ip) do
+    case :ets.lookup(@table_name, ip) do
+      [{^ip, requests}] -> requests
+      _ -> []
+    end
+  end
 
-    current_requests = [current_time | recent_request_times]
-    new_state = Map.put(state, ip, current_requests)
+  defp filter_recent_request_times(current_time, request_times) do
+    end_time = Timex.subtract(current_time, @time_period_minutes)
 
-    {:reply, length(current_requests) <= @limit, new_state}
+    Enum.filter(request_times, fn time ->
+      Timex.between?(time, end_time, current_time, inclusive: true)
+    end)
   end
 end
