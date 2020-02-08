681f2b938bbfbceb0b7e4e7ab4c8f4b9bedb0042
Jake Worth <dev+jwworth@hashrocket.com>
2020-02-08 11:36:12 -0600

    Add more tests

--- lib/tilex_web/views/shared_view.ex
+++ lib/tilex_web/views/shared_view.ex
@@ -17,12 +17,13 @@ defmodule TilexWeb.SharedView do
     Timex.format!(inserted_at, "%B %-e, %Y", :strftime)
   end
 
+  def pluralize(1, object), do: "1 #{object}"
+
+  def pluralize(count, object), do: "#{count} #{object}s"
+
   def rss_date(post) do
     Timex.format!(post.inserted_at, "%a, %d %b %Y %H:%M:%S GMT", :strftime)
   end
-
-  def pluralize(1, object), do: "1 #{object}"
-  def pluralize(count, object), do: "#{count} #{object}s"
 
   def pagination_href(conn, page) do
     conn.request_path <> "?" <> URI.encode_query(Map.put(conn.params, "page", page))
