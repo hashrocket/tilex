5326a45469fdfdf5bab2ceb945d3950435a8ea33
Jake Worth <dev+jwworth@hashrocket.com>
2020-02-08 12:03:02 -0600

    Extract TZ conversion for sharing

--- lib/tilex_web/views/shared_view.ex
+++ lib/tilex_web/views/shared_view.ex
@@ -5,16 +5,9 @@ defmodule TilexWeb.SharedView do
   alias Timex.Timezone
 
   def display_date(post) do
-    inserted_at =
-      case Application.get_env(:tilex, :date_display_tz) do
-        x when x in [nil, ""] ->
-          post.inserted_at
-
-        tz ->
-          Timezone.convert(post.inserted_at, tz)
-      end
-
-    Timex.format!(inserted_at, "%B %-e, %Y", :strftime)
+    post.inserted_at
+    |> maybe_convert_to_display_tz()
+    |> Timex.format!("%B %-e, %Y", :strftime)
   end
 
   def pluralize(1, object), do: "1 #{object}"
@@ -32,5 +25,15 @@ defmodule TilexWeb.SharedView do
   def post_creator_or_admin?(conn, post) do
     Plug.current_resource(conn) &&
       (post.developer == Plug.current_resource(conn) || Plug.current_resource(conn).admin)
+  end
+
+  defp maybe_convert_to_display_tz(timestamp) do
+    case Application.get_env(:tilex, :date_display_tz) do
+      setting when setting in [nil, ""] ->
+        timestamp
+
+      tz ->
+        Timezone.convert(timestamp, tz)
+    end
   end
 end
