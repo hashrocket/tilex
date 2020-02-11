6dbbd8312b13179742b6caa31d98e6b5e1a22e36
Jake Worth <jake@jakeworth.com>
2020-02-10 18:03:32 -0600

    Add Byline and Post Time Tag (#650)
    * Add more tests
    
    * Don't rely on env TZ, set it in test
    
    * Extract TZ conversion for sharing
    
    * Wrap author and date in Reader Mode friendly tags

--- lib/tilex_web/views/shared_view.ex
+++ lib/tilex_web/views/shared_view.ex
@@ -5,24 +5,18 @@ defmodule TilexWeb.SharedView do
   alias Timex.Timezone
 
   def display_date(post) do
-    inserted_at =
-      case Application.get_env(:tilex, :date_display_tz) do
-        x when x in [nil, ""] ->
-          post.inserted_at
-
-        tz ->
-          Timezone.convert(post.inserted_at, tz)
-      end
-
-    Timex.format!(inserted_at, "%B %-e, %Y", :strftime)
+    post.inserted_at
+    |> maybe_convert_to_display_tz()
+    |> Timex.format!("%B %-e, %Y", :strftime)
   end
+
+  def pluralize(1, object), do: "1 #{object}"
+
+  def pluralize(count, object), do: "#{count} #{object}s"
 
   def rss_date(post) do
     Timex.format!(post.inserted_at, "%a, %d %b %Y %H:%M:%S GMT", :strftime)
   end
-
-  def pluralize(1, object), do: "1 #{object}"
-  def pluralize(count, object), do: "#{count} #{object}s"
 
   def pagination_href(conn, page) do
     conn.request_path <> "?" <> URI.encode_query(Map.put(conn.params, "page", page))
@@ -31,5 +25,15 @@ defmodule TilexWeb.SharedView do
   def post_creator_or_admin?(conn, post) do
     Plug.current_resource(conn) &&
       (post.developer == Plug.current_resource(conn) || Plug.current_resource(conn).admin)
+  end
+
+  defp maybe_convert_to_display_tz(timestamp) do
+    case Application.get_env(:tilex, :date_display_tz) do
+      setting when setting in [nil, ""] ->
+        timestamp
+
+      tz ->
+        Timezone.convert(timestamp, tz)
+    end
   end
 end
--- test/views/shared_view_test.exs
+++ test/views/shared_view_test.exs
@@ -3,8 +3,27 @@ defmodule Tilex.SharedViewTest do
 
   import TilexWeb.SharedView
 
-  test "renders rfc 822 dates" do
-    time = %{inserted_at: ~N[2002-10-02 08:00:00.142287]}
-    assert rss_date(time) == "Wed, 02 Oct 2002 08:00:00 GMT"
+  @insert_time %{inserted_at: ~N[2016-11-18 03:34:08.142287]}
+
+  describe "display_date/1" do
+    test "it produces a human-readable date in server's timezone" do
+      assert display_date(@insert_time) == "November 17, 2016"
+    end
+  end
+
+  describe "pluralize/2" do
+    test "counts 1" do
+      assert pluralize(1, "hippo") == "1 hippo"
+    end
+
+    test "counts 2" do
+      assert pluralize(2, "hippo") == "2 hippos"
+    end
+  end
+
+  describe "rss_date/1" do
+    test "renders rfc 822 dates" do
+      assert rss_date(@insert_time) == "Fri, 18 Nov 2016 03:34:08 GMT"
+    end
   end
 end
