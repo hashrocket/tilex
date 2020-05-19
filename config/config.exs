cd502cd23c7af56c1b5abb490bf32ee5c0cdf67e
Hashrocket Workstation <dev@hashrocket.com>
2020-05-19 16:27:56 -0500

    Split configuration into runtime readable file

--- config/config.exs
+++ config/config.exs
@@ -24,27 +24,6 @@ config :tilex, :page_size, 5
 config :tilex, :auth_controller, AuthController
 config :tilex, :slack_notifier, Tilex.Notifications.Notifiers.Slack
 config :tilex, :twitter_notifier, Tilex.Notifications.Notifiers.Twitter
-config :tilex, :organization_name, System.get_env("ORGANIZATION_NAME")
-config :tilex, :canonical_domain, System.get_env("CANONICAL_DOMAIN")
-config :tilex, :default_twitter_handle, System.get_env("DEFAULT_TWITTER_HANDLE")
-config :tilex, :cors_origin, System.get_env("CORS_ORIGIN")
-config :tilex, :hosted_domain, System.get_env("HOSTED_DOMAIN")
-config :tilex, :guest_author_whitelist, System.get_env("GUEST_AUTHOR_WHITELIST")
-config :tilex, :date_display_tz, System.get_env("DATE_DISPLAY_TZ")
-config :tilex, :imgur_client_id, System.get_env("IMGUR_CLIENT_ID")
-
-config :tilex,
-       :rate_limiter_requests_per_time_period,
-       System.get_env("RATE_LIMIT_REQUESTS_PER_TIME_PERIOD") || 5
-
-config :tilex,
-       :rate_limiter_time_period_minutes,
-       System.get_env("RATE_LIMIT_TIME_PERIOD_MINUTES") || 1
-
-config :tilex,
-       :rate_limiter_cleanup_interval,
-       # every 10 hours
-       System.get_env("RATE_LIMIT_CLEANUP_INTERVAL") || 10 * 60 * 60_000
 
 config :tilex, :date_time_module, DateTime
 
@@ -64,10 +43,6 @@ config :ueberauth, Ueberauth,
        ]}
   ]
 
-config :ueberauth, Ueberauth.Strategy.Google.OAuth,
-  client_id: System.get_env("GOOGLE_CLIENT_ID"),
-  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
-
 config :tilex, Tilex.Auth.Guardian,
   issuer: "tilex",
   ttl: {30, :days},
@@ -78,12 +53,7 @@ config :tilex, Tilex.Auth.Guardian,
     "kty" => "oct"
   }
 
-config :extwitter, :oauth,
-  consumer_key: System.get_env("twitter_consumer_key"),
-  consumer_secret: System.get_env("twitter_consumer_secret"),
-  access_token: System.get_env("twitter_access_token"),
-  access_token_secret: System.get_env("twitter_access_token_secret")
-
+import_config "env_vars.exs"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- config/releases.exs
+++ config/releases.exs
@@ -4,3 +4,5 @@ config :tilex, TilexWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+
+import_config "env_vars.exs"
