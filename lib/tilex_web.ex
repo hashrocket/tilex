513ae4e01ad22752b0ac652dfbe03dcd502084a0
Hashrocket Workstation <dev@hashrocket.com>
2021-06-25 16:59:44 -0400

    Bump hex packages
    * resolves deprecation warnings from Phoenix, updates PubSub server
      configuration
    * locks Earmark to 1.4.1 -> updating to most recent version (1.4.15)
      breaks multiple features
    * prefer sanitizing of html first before parsing markdown
    * use `appsignal_phoenix` over `appsignal`
    * various style updates
    
    Co-authored-by: Vinicius Negrisolo <vinicius.negrisolo@hashrocket.com>
    Co-authored-by: Andrew Vogel <andrew.vogel@hashrocket.com>

--- lib/tilex_web.ex
+++ lib/tilex_web.ex
@@ -45,6 +45,8 @@ defmodule TilexWeb do
         root: "lib/tilex_web/templates",
         namespace: TilexWeb
 
+      use Appsignal.Phoenix.View
+
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
 
