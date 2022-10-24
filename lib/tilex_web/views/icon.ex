70916f65356a621e365709868c43be5a39bf44fe
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2022-10-24 16:07:55 -0400

    Refresh error layout to use new svg icons

--- lib/tilex_web/views/icon.ex
+++ lib/tilex_web/views/icon.ex
@@ -5,14 +5,10 @@ defmodule TilexWeb.Icon do
   @icons_svg_file "images/icons.svg"
   @external_resource "priv/static/#{@icons_svg_file}"
   @icons_svg_content File.read!(@external_resource)
+  @icon_names ~r/id="([\w-]+)"/ |> Regex.scan(@icons_svg_content) |> Enum.map(&List.last/1)
+  @sizes [:small, :medium, :large]
 
-  @icon_names ~r/id="([\w-]+)"/
-              |> Regex.scan(@icons_svg_content)
-              |> Enum.map(&List.last/1)
-
-  @sizes [:small, :regular, :large]
-
-  def icon(name, size \\ :regular, title \\ nil) when size in @sizes do
+  def icon(name, size \\ :medium, title \\ nil) when name in @icon_names and size in @sizes do
     content_tag(:svg, class: "icon icon-#{name} icon-#{size}", aria_labelledby: "title") do
       [
         content_tag(:title, title || name, lang: "en"),
@@ -21,7 +17,5 @@ defmodule TilexWeb.Icon do
     end
   end
 
-  defp icon_path(name) when name in @icon_names do
-    static_path(TilexWeb.Endpoint, "/#{@icons_svg_file}") <> "##{name}"
-  end
+  defp icon_path(name), do: static_path(TilexWeb.Endpoint, "/#{@icons_svg_file}") <> "##{name}"
 end
