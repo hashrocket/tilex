e0de631ee493cbebea4cf96a7e62929ab3e2c5ad
Andrew Vogel <andrew.vogel@hashrocket.com>
2025-05-04 13:46:26 -0400

    chore: update esbuild version for nested css support

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ config :swoosh, :api_client, false
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.29",
+  version: "0.18.6",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
