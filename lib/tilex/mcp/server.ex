0c45f282bbf7ea6ccb1c35556e24d24c47f74a43
Hashrocket Workstation <dev@hashrocket.com>
2025-10-27 15:41:43 -0400

    Assign current user into mcp server
    Co-authored-by: Vinicius Negrisolo <vinicius.negrisolo@hashrocket.com>
    Co-authored-by: Gabriel Reis <gabriel.reis@hashrocket.com>

--- lib/tilex/mcp/server.ex
+++ lib/tilex/mcp/server.ex
@@ -1,8 +1,24 @@
 defmodule Tilex.MCP.Server do
-  use Hermes.Server,
-    name: "TIL",
-    version: "1.0.0",
-    capabilities: [:tools]
+  use Hermes.Server, name: "TIL", version: "1.0.0", capabilities: [:tools]
+
+  import Ecto.Query, only: [from: 2]
+
+  alias Tilex.Repo
+  alias Tilex.Blog.Developer
 
   component(Tilex.MCP.NewPost)
+
+  def init(arg, frame) do
+    headers = Enum.into(frame.transport.req_headers, %{})
+    user = get_current_user(headers["x-api-key"])
+    assigns = Map.put(frame.assigns, :current_user, user)
+    frame = Map.put(frame, :assigns, assigns)
+    {:ok, frame}
+  end
+
+  defp get_current_user("" <> _ = api_key) do
+    Repo.one(from d in Developer, where: d.id == ^api_key)
+  end
+
+  defp get_current_user(_api_key), do: nil
 end
