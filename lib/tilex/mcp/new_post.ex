83b47ae40de33719ddd206f4558260f433817979
Hashrocket Workstation <dev@hashrocket.com>
2025-10-28 10:13:33 -0400

    Create TIL from MCP tool
    Co-authored-by: Vinicius Negrisolo <vinicius.negrisolo@hashrocket.com>
    Co-authored-by: Gabriel Reis <gabriel.reis@hashrocket.com>

--- lib/tilex/mcp/new_post.ex
+++ lib/tilex/mcp/new_post.ex
@@ -1,24 +1,51 @@
 defmodule Tilex.MCP.NewPost do
-  @moduledoc "Post new TIL"
+  @moduledoc """
+  Create a new TIL ("Today I Learned") post.
+
+  TIL is a place for sharing something you've learned today with others.
+  """
 
   use Hermes.Server.Component, type: :tool
 
   alias Hermes.Server.Response
+  alias Tilex.Blog.Developer
+  alias Tilex.Blog.Post
+  alias TilexWeb.Endpoint
+  alias TilexWeb.Router.Helpers, as: Routes
 
   schema do
-    field :title, :string, required: true
-    field :content, :string, required: true
+    field :title, :string,
+      required: true,
+      description: "Max #{Post.title_max_chars()} chars."
+
+    field :body, :string,
+      required: true,
+      description:
+        "Max #{Post.body_max_words()} words in a Markdown format."
   end
 
-  def execute(%{title: _title, content: _content}, frame) do
-    response =
-      Response.tool()
-      |> Response.resource_link(
-        "https://til.hashrocket.com/posts/nlxtvqyfl3-check-your-shell-scripts-with-shellcheck",
-        "til-post",
-        description: "Link to the TIL post preview"
-      )
+  def execute(input, frame) do
+    current_user = frame.assigns.current_user
+    resp = Response.tool()
 
-    {:reply, response, frame}
+    resp =
+      case create_til_post(current_user, input) do
+        {:ok, %Post{} = post} ->
+          url = Routes.post_url(Endpoint, :show, post)
+          Response.resource_link(resp, url, "til-post", description: "Link to the TIL post preview")
+
+        {:error, reason} ->
+          Response.error(resp, "ERROR => #{reason}")
+      end
+
+    {:reply, resp, frame}
+  end
+
+  defp create_til_post(nil, _input) do
+    {:error, "User is not authenticated to create TILs"}
+  end
+
+  defp create_til_post(%Developer{} = current_user, %{title: title, body: body}) do
+    {:ok, %Post{title: title, body: body, slug: "foo-bar"}}
   end
 end
