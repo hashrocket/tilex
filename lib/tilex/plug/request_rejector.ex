3fda6bcf22c62a3951d6183580c0cb2d3715e68e
Hashrocket Workstation <dev@hashrocket.com>
2025-10-30 13:39:48 -0400

    Downgrade to elixir 1.18.4
    Co-authored-by: Vinicius Negrisolo <vinicius.negrisolo@hashrocket.com>
    Co-authored-by: Gabriel Reis <gabriel.reis@hashrocket.com>

--- lib/tilex/plug/request_rejector.ex
+++ lib/tilex/plug/request_rejector.ex
@@ -1,12 +1,8 @@
 defmodule Tilex.Plug.RequestRejector do
-  @rejected_paths [
-    ~r/\.php$/
-  ]
-
   def init([]), do: []
 
   def call(%Plug.Conn{request_path: path} = conn, _default) do
-    if Enum.any?(@rejected_paths, &match_rejected_path(path, &1)) do
+    if Enum.any?([~r/\.php$/], &match_rejected_path(path, &1)) do
       TilexWeb.ErrorView.render_error_page(conn, 404)
     else
       conn
